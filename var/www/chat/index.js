d2c5aff682ba1a989c3b92594f2a4ab8244534d8
Admin <admin@feministwiki.org>
2019-04-22 22:32:26 +0200

    Update chat html and js.

--- var/www/chat/index.js
+++ var/www/chat/index.js
@@ -1,5 +1,5 @@
 converse.initialize({
-    view_mode: 'fullscreen',
+    view_mode: "fullscreen",
     websocket_url: "wss://xmpp.feministwiki.org:5280/websocket",
     locked_domain: "feministwiki.org",
     muc_domain: "groups.feministwiki.org",
@@ -9,8 +9,17 @@ converse.initialize({
 document.addEventListener("DOMContentLoaded", function() {
     var heading = document.getElementsByClassName("brand-heading")[0]
     heading.innerHTML = "FeministChat"
+
     var subtitles = document.getElementsByClassName("brand-subtitle")
     while (subtitles.length > 0) {
         subtitles[0].parentNode.removeChild(subtitles[0])
+    }
+
+    var labels = document.getElementsByTagName("label")
+    for (var i = 0; i < labels.length; ++i) {
+        if (labels[i].htmlFor = "converse-login-jid") {
+            labels[i].innerHTML = "Username:"
+            break
+        }
     }
 })
