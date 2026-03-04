23d0621bc30007da7d820a48a5c7362264bfe9da
Taylan Kammer <taylan.kammer@gmail.com>
2026-03-04 14:32:18 +0100

    update

--- update.sh
+++ update.sh
@@ -22,6 +22,7 @@ find etc root var -type f -exec sh -c 'for file; do cp -a /"$file" "$file"; done
 
 sed -ri 's/(ldap_password): "[^"]+"/\1: "[REDACTED]"/' etc/ejabberd/ejabberd.yml
 sed -ri 's/(key|password|bindauth)="[^"]+"/\1="[REDACTED]"/' etc/inspircd/inspircd.conf
+sed -ri 's/(password) = "[^"]+"/\1 = "[REDACTED]"/' etc/rspamd/local.d/worker-controller.inc
 
 if [ "$passwords" ]
 then
